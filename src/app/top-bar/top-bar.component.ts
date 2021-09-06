d65f0cd196c06a930403bc5dd9f9b242bd1a6219
josarnaiz <josarnaiz@gmail.com>
2021-09-06 18:26:00 +0200

    show checkout items

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
+  constructor(
+    private cartService: CartService
+  ) { 
+
+  }
+
+  getItems() {
+    return this.cartService.getItems();
+  }
 
 }
 
